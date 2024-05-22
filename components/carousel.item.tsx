b2f40c88cb9391f265b508ea764fc6b1949e16e6
Qousul <sonicthehedgehog2961@gmail.com>
2024-05-22 08:29:52 +0700

    ngganti warna - warna untuk dark mode

--- components/carousel.item.tsx
+++ components/carousel.item.tsx
@@ -5,6 +5,7 @@ import {
 import { fontSize, hijauRamadhan } from 'styles/theme';
 import { AccessibilityContext } from 'contexts/accessibility';
 import { BreakpointsContext } from 'contexts/breakpoints';
+import { ColorModeContext } from 'contexts/colorMode';
 import useTextToSpeech from 'hooks/useTextToSpeech';
 
 interface Props {
@@ -21,6 +22,7 @@ const CarouselItem: React.FunctionComponent<Props> = ({
   const accessibility = React.useContext(AccessibilityContext);
   const { downMd } = React.useContext(BreakpointsContext);
   const { downSm } = React.useContext(BreakpointsContext);
+  const { mode } = React.useContext(ColorModeContext);
   const { textToSpeech } = useTextToSpeech();
   const handleClick = React.useCallback(() => {
     if (onClick) {
@@ -34,14 +36,15 @@ const CarouselItem: React.FunctionComponent<Props> = ({
       justifyContent:`center`,
       alignItems:`center`,
       textAlign:`center`,
-      borderLeft:`${text === `Bisnis dan Investasi` ? `1px` : `0px`} solid #003f5a`,
-      borderRight:`${text === `Bisnis dan Investasi` ? `1px` : `0px`} solid #003f5a`,
+      borderLeft:`${text === `Bisnis dan Investasi` ? `1px` : `0px`} solid ${mode === 'light'? hijauRamadhan : `white`}`,
+      borderRight:`${text === `Bisnis dan Investasi` ? `1px` : `0px`} solid ${mode === 'light'? hijauRamadhan : `white`}`,
       }}>
       
-      <img src={src} style={{
+      <object type="image/svg+xml" data={src} id="svg-object" style={{
         maxHeight: downMd ? `2rem` : `5rem`,
-        marginBottom: downMd? `1rem` : `3rem`
-      }}/>
+        marginBottom: downMd? `1rem` : `3rem`,
+        fill : `white`
+      }}></object>
 
       {text &&
         <Typography
@@ -49,7 +52,7 @@ const CarouselItem: React.FunctionComponent<Props> = ({
           style={{
             fontSize:downSm?`10pt`:(fontSize + 2) + accessibility.fontSize  
           }}
-          color={hijauRamadhan}
+          color={mode === 'light'? hijauRamadhan : `white`}
         >
           {text}
         </Typography>
